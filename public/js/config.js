0f5d8e5d95cdc8ad7b4dbd5e8e9988274811c253
Brian Huang <b01902068@ntu.edu.tw>
2016-05-07 07:38:19 +0800

    rm useless

--- public/js/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	FIREBASE_URL: 'https://eztable-firebase.firebaseio.com',
-	CLOUDAMQP_URL: 'amqp://iagsgtqu:e4BRZ_jfoObcNWQX7rvaBFx4fafO-YU3@moose.rmq.cloudamqp.com/iagsgtqu',
-	PORT: 15002,
-};
