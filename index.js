48bb7ad72f3d4d5d10177112bc64e83fae939d0b
Brian Huang <b01902068@ntu.edu.tw>
2016-05-09 21:10:59 +0800

    fix

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "127.0.0.1:8080");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
