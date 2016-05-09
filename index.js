daa756484e5dd8d66fcb10a71115b6fa5fe22b06
Brian Huang <b01902068@ntu.edu.tw>
2016-05-09 21:46:34 +0800

    fix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static(__dirname + '/hour'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET");
+  //res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
