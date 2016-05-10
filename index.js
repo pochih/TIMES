90622238bd34e27a57b68f07f36134046f15db79
Brian Huang <b01902068@ntu.edu.tw>
2016-05-11 00:44:09 +0800

    fix

--- index.js
+++ index.js
@@ -101,6 +101,8 @@ app.get('/user/dead', function(req, res) {
 app.get('/user/init', function(req, res) {
   var userdb = db.user;
   var user = parser.initUser(userdb, req.query);
+  if (req.query.id == '0')
+    user.timeLeft.hours = 40000;
   USER.child(user._id).set(user);
 
   var tmp = user.timeLeft;
