bf95dcdc6f571e0666c13683b4ae2457c25850e8
Brian Huang <b01902068@ntu.edu.tw>
2016-05-21 15:08:04 +0800

    fix

--- db.js
+++ db.js
@@ -126,7 +126,7 @@ module.exports = {
   		'https://art-festival.herokuapp.com/user/init?a=1&p=-1&i=0&name=Che Guevara&id=4&device=4',
   		'https://art-festival.herokuapp.com/user/init?a=4&p=-2&i=-2&name=Albert Einstein&id=5&device=5',
   		'https://art-festival.herokuapp.com/user/init?a=-1&p=3&i=-2&name=Donald John Trump&id=6&device=6',
-  		'https://art-festival.herokuapp.com/user/init?a=0&p=0&i=0&name=Hello Kitty&id=7&device=7',
+  		'https://art-festival.herokuapp.com/user/init?a=-2&p=2&i=0&name=Hello Kitty&id=7&device=7',
   		'https://art-festival.herokuapp.com/user/init?a=-2&p=2&i=0&name=%E5%AD%94%E4%B8%98&id=8&device=8',
   		'https://art-festival.herokuapp.com/user/init?a=0&p=-1&i=1&name=%E5%A7%9A%E6%98%8E&id=9&device=9',
   		'https://art-festival.herokuapp.com/user/init?a=0&p=0&i=0&name=%E6%9D%91%E4%B8%8A%E6%98%A5%E6%A8%B9&id=10&device=10',
